0d7f237679411f5c8862b24d83edb154c440309f
Edmund Anglero <edmund.jr.anglero@gmail.com>
2020-07-31 03:38:57 -0400

    did some changes

--- another.cpp
+++ another.cpp
@@ -2,18 +2,23 @@
 
 #include <iostream>
 using namespace std;
+
+class A
+{
+public:
+	A()
+	{
+		cout << "running constructor" << endl;
+	}
+	~A()
+	{
+		cout << "running destructor" << endl;
+	}
+};
+
 int main()
 {
-	int age;
-	cout << "How old are you? ";
-	cin >> age;
-
-	if(age >= 21)
-		cout << "You can drink and smoke." << endl;
-	if(age >= 16)
-		cout << "You can drive." << endl;
-	if(age < 16)
-		cout << "Sorry, not much you can do yet." << endl;
+	A foo;
 
 	return 0;
 }
